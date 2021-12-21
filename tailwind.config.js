40cf8339aa46fe460cde894abdeeb4f26a2c2bda
Sunil <skumar@constellant.com>
2021-12-21 12:17:10 +0530

    growth-webflow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
+    color:{
+      primary:{
+        10:'#f4263e',
+      },
+
+      secondary:{
+        10:'#62668a',
+      },
+
+      info:{
+        10:'#1f2559',
+      }
+    },
     extend: {},
   },
   plugins: [],
