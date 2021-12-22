fe3c6fa5f1d9b281ba2ddc1db93bd14ecfffd68d
Sunil <skumar@constellant.com>
2021-12-22 18:58:09 +0530

    growth-webflow

--- public/main.js
+++ public/main.js
@@ -1,8 +1,26 @@
 // ==== Mobilemenu Navbar
-
-function toggle(){
+function toggle() {
     var dropdown = document.getElementById('toggleMenu');
-    var  menu = document.getElementById('dropdownMenu');
+    var menu = document.getElementById('dropdownMenu');
     dropdown.classList.toggle('hidden');
     menu.classList.add('left-0');
 }
+
+// banner image on scroll moments.
+window.onscroll = function () {
+    var grow1 = document.getElementById('growSvgOne');
+    var banner = document.getElementById('growBanner');
+    var grow2 = document.getElementById('growSvgTwo');
+
+    if (window.scrollY > 50) {
+        grow1.classList.add('-translate-y-7');
+        banner.classList.add('translate-y-7');
+        grow2.classList.add('-translate-y-7');
+    }
+
+    else if(window.scrollY == 0){
+        grow1.classList.remove('-translate-y-7');
+        banner.classList.remove('translate-y-7');
+        grow2.classList.remove('-translate-y-7');
+    }
+};
