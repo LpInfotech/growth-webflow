372610c22487d762ec7330dca3d400441c0a76dc
Sunil <skumar@constellant.com>
2022-01-05 18:07:07 +0530

    growth-webflow

--- public/main.js
+++ public/main.js
@@ -16,22 +16,39 @@ window.onscroll = function () {
         grow1.classList.add('-translate-y-7');
         banner.classList.add('translate-y-7');
         grow2.classList.add('-translate-y-7');
-    }
-
-    else if(window.scrollY == 0){
+    } else if (window.scrollY == 0) {
         grow1.classList.remove('-translate-y-7');
         banner.classList.remove('translate-y-7');
         grow2.classList.remove('-translate-y-7');
     }
 };
 
-// tabs are change on click
-$(document).ready(function(){
-    $('.tab-link-items').click(function(){
+// tabs are change onclick
+$(document).ready(function () {
+    $('.tabs-menu:first').removeClass('hidden');
+    $('.tab-link-items:first').addClass('border-primary');
+
+    $('.tab-link-items').click(function () {
         $('.tab-link-items').removeClass('active');
         $(this).addClass('active');
+
         $('.tab-link-items').removeClass('border-primary');
         $(this).addClass('border-primary');
-        
+
+        if($('#tabLink1').hasClass('active')){
+            $('.tabs-menu').addClass('hidden')
+            $("#tabMenu1").removeClass('hidden');
+        }
+
+        else if($('#tabLink2').hasClass('active')){
+            $('.tabs-menu').addClass('hidden')
+            $("#tabMenu2").removeClass('hidden');
+        }
+
+        else if($('#tabLink3').hasClass('active')){
+            $('.tabs-menu').addClass('hidden')
+            $("#tabMenu3").removeClass('hidden');
+        }
+
     });
 });
